797ab53e1ed299f3c6206af8de1748ecfe2981f9
ManasaGottimukkula <17wh1a05b6@bvrithyderabad.edu.in>
2020-03-04 23:02:27 +0530

    modified classes

--- CollectionFramework/src/main/java/com/epam/CollectionFramework/MyList.java
+++ CollectionFramework/src/main/java/com/epam/CollectionFramework/MyList.java
@@ -13,7 +13,9 @@ public class MyList<T> {
     }
 	
 	public void add(T t) {
-		
+		if (listSize == listValues.length) {
+            Capacity();
+        }
         listValues[listSize++] = t;
     }
 	
@@ -21,18 +23,36 @@ public class MyList<T> {
 		return listSize;
 	}
 	
-
-
+	public T remove(int i) {
+	    if (i > listSize || i < 0) {
+	        throw new IndexOutOfBoundsException("Of index: " + i + "& size =" + i);
+	    }
+	    Object value = listValues[i];
+	    int remlen = listValues.length - (i) ;
+	    System.arraycopy( listValues, i , listValues, i-1, remlen ) ;
+	    listSize--;
+	    return (T) value;
+	}
+	 
+	public T getElementByIndex(int i) {
+		if (i > listSize || i < 0) {
+	        throw new IndexOutOfBoundsException("Of index: " + i + "& size " + i);
+	    }
+		return (T) listValues[i-1];
+	}
+	 
+	private void Capacity() {
+        int expSize = listValues.length * 2;
+        listValues = Arrays.copyOf(listValues, expSize);
+    }
 	
-	  @Override
-	    public String toString() 
-	    {
-			String str= "";
-	         str += '[';
-	         for(int i = 0; i < listSize ;i++) {
-	             str += listValues[i].toString();
-	             if(i<listSize-1){
-	                 str += ",";
+	@Override
+	public String toString() {
+		String str= "[";
+	    for(int i = 0; i < listSize ;i++) {
+	    	str += listValues[i].toString();
+	        	if(i < listSize-1){
+	                 str += " , ";
 	             }
 	         }
 	         str += ']';
--- CollectionFramework/src/main/java/com/epam/CollectionFramework/MyListImplementation.java
+++ CollectionFramework/src/main/java/com/epam/CollectionFramework/MyListImplementation.java
@@ -5,11 +5,26 @@ public class MyListImplementation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyList<String> ml = new MyList<String>();
-		ml.add("Manasa");
-		ml.add("sindhu");
-		ml.add("Kavya");
-		System.out.println(ml);
-		System.out.println(ml.size());
+		//adding elements
+		ml.add("Delhi");
+		ml.add("Mumbai");
+		ml.add("Hyderabad");
+		ml.add("India");
+		ml.add("Chennai");
+		ml.add("Banglore");
+		
+		//printing list and size
+		System.out.println("List is "+ ml);
+		System.out.println("Size of the list is " +ml.size());
+		
+		//removing an element
+		ml.remove(4);
+		System.out.println("After removing, the list is "+ml);
+		System.out.println("Size of the list is " + ml.size());
+		
+		//fetching the element by index
+		System.out.println(ml.getElementByIndex(4));
+
 	}
 
 }
