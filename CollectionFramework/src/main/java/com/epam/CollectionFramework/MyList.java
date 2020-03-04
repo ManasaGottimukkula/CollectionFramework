ed0174e7d39c57842ca44b628a17ce4f305ef8cf
ManasaGottimukkula <17wh1a05b6@bvrithyderabad.edu.in>
2020-03-04 21:01:44 +0530

    created classes

---
+++ CollectionFramework/src/main/java/com/epam/CollectionFramework/MyList.java
@@ -0,0 +1,5 @@
+package com.epam.CollectionFramework;
+
+public class MyList {
+
+}
---
+++ CollectionFramework/src/main/java/com/epam/CollectionFramework/MyListImplementation.java
@@ -0,0 +1,10 @@
+package com.epam.CollectionFramework;
+
+public class MyListImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
