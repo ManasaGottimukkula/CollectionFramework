f6b81cfcaf7a14870596b073c4768250a36fd7d2
ManasaGottimukkula <17wh1a05b6@bvrithyderabad.edu.in>
2020-03-04 21:38:24 +0530

    adding methods of add and size functions

--- CollectionFramework/src/main/java/com/epam/CollectionFramework/MyList.java
+++ CollectionFramework/src/main/java/com/epam/CollectionFramework/MyList.java
@@ -1,19 +1,42 @@
 package com.epam.CollectionFramework;
 
+import java.util.Arrays;
+
 public class MyList<T> {
 	
-	public int listSize = 0;
-	public static final int InitialSIZE = 10;
-	public Object listValues[];
+	private int listSize = 0;
+	private static final int InitialSIZE = 10;
+	private Object listValues[];
 	
 	public MyList() {
 		listValues = new Object[InitialSIZE];
     }
 	
+	public void add(T t) {
+		
+        listValues[listSize++] = t;
+    }
 	
 	public int size() {
 		return listSize;
 	}
 	
 
+
+	
+	  @Override
+	    public String toString() 
+	    {
+			String str= "";
+	         str += '[';
+	         for(int i = 0; i < listSize ;i++) {
+	             str += listValues[i].toString();
+	             if(i<listSize-1){
+	                 str += ",";
+	             }
+	         }
+	         str += ']';
+	         return str;
+	    }
+	
 }
--- CollectionFramework/src/main/java/com/epam/CollectionFramework/MyListImplementation.java
+++ CollectionFramework/src/main/java/com/epam/CollectionFramework/MyListImplementation.java
@@ -5,8 +5,11 @@ public class MyListImplementation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyList<String> ml = new MyList<String>();
-		
-
+		ml.add("Manasa");
+		ml.add("sindhu");
+		ml.add("Kavya");
+		System.out.println(ml);
+		System.out.println(ml.size());
 	}
 
 }
